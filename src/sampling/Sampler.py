87d682320515f6ed6a07624b49b508cfff81f84c
Bogdan Bîndilă <b.mihaibogdan@yahoo.com>
2024-02-01 09:23:45 +0100

    fix the sampling that did not work properly

--- src/sampling/Sampler.py
+++ src/sampling/Sampler.py
@@ -9,24 +9,26 @@ class Sampler:
         self.generator = np.random.default_rng(seed)
 
     def sample(
-        self, percentage: float, eval_percentage=0.05
+        self, percentage: float, eval_percentage=0.2
     ) -> tuple[np.ndarray, np.ndarray]:
         """
         Returns a fraction of the training set.
         Returns equal amount of 'private' images from the test set to calculate a model's losses.
         Images are identified by their index in the dataset. Valid indexes range from 0 to 49999.
         """
-        if percentage > 20.0 or percentage < 0:
-            raise Exception("invalid percentage. must be between 0 and 20.")
+        if percentage > 16.0 or percentage <= 0:
+            raise Exception("invalid percentage. must be between 0 and 16.")
 
         sample_size = int((TRAIN_SIZE * percentage) / 100)
-
-        train_idx = self.generator.choice(TRAIN_SIZE - 1, sample_size, replace=False)
-        test_idx = self.generator.choice(TEST_SIZE - 1, sample_size, replace=False)
-
         size_eval = int(sample_size * eval_percentage)
 
-        eval_train_idx = train_idx[:-size_eval]
-        eval_test_idx = test_idx[:-size_eval]
+        train_idx = self.generator.choice(TRAIN_SIZE - 1, sample_size + size_eval, replace=False)
+        test_idx = self.generator.choice(TEST_SIZE - 1, sample_size + size_eval, replace=False)
+
+        eval_train_idx = train_idx[-size_eval:]
+        eval_test_idx = test_idx[-size_eval:]
+
+        train_idx = train_idx[:-size_eval]
+        test_idx = test_idx[:-size_eval]
 
         return train_idx, test_idx, eval_train_idx, eval_test_idx
